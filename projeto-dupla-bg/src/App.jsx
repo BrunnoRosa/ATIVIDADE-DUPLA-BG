d780274819f3395d04b2e5235773a5c7692ff644
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-09 21:25:06 -0300

    Mudanças no APP.JSX

--- projeto-dupla-bg/src/App.jsx
+++ projeto-dupla-bg/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import PageHome from './pages/PageHome'
 import './App.css'
 import Footer from './components/Footer'
+import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <h1>Atividade em Dupla - Bruno & Geovane</h1>  
-    <Footer/>  
+    <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<PageHome />}/>
+          <Route path='/servicos' element={<Servicos />}/>
+        </Routes>
+      </main>
+        
+    <h1>Atividade em Dupla - Bruno & Geovane</h1>
+    <Footer/>
     
     </>
   )
