2ff7488b489b0857c470d7ec4eb69467f92ca365
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-09 21:07:40 -0300

    alteração Header

--- projeto-dupla-bg/src/components/Header/index.jsx
+++ projeto-dupla-bg/src/components/Header/index.jsx
@@ -1,1 +1,29 @@
 import './style.css'
+import { Link, NavLink } from 'react-router-dom'
+
+
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <Link to="/" className='header-logo'>
+                <h1>MY ANIME LIST</h1>
+                </Link>
+            </div>
+            <nav>
+                <NavLink>
+                    <Link to="/">HOME</Link>
+                </NavLink>
+                <NavLink>
+                    <Link to="/produtos">PRODUTOS</Link>
+                </NavLink>
+                <NavLink>
+                    <Link to="/servicos">SERVIÇOS</Link>
+                </NavLink>
+                <NavLink>
+                    <Link to="/sobre-nos">SOBRE NÓS</Link>
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
